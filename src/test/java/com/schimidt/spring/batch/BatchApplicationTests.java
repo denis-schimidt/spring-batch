f979815b1b16b21d42bcc1126708a5536830d08a
Dênis Schimidt de Oliveira <denis.schimidt@gmail.com>
2024-07-11 13:24:20 -0300

    Adiciona teste integrado para o Spring Batch

--- src/test/java/com/schimidt/spring/batch/BatchApplicationTests.java
+++ src/test/java/com/schimidt/spring/batch/BatchApplicationTests.java
@@ -1,13 +1,33 @@
 package com.schimidt.spring.batch;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@SpringBatchTest
 @SpringBootTest
 class BatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testCoffeeJob(@Autowired Job job) throws Exception {
+		this.jobLauncherTestUtils.setJob(job);
+		var jobExecution = jobLauncherTestUtils.launchJob();
+		Assertions.assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+
+		// Verify the data in the database
+		var count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM coffee", Integer.class);
+		Assertions.assertTrue(count > 0, "There should be at least one record in the database");
+	}
 }
